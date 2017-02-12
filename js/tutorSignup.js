bc666697b8cfd8364ed9eff49ef0923295ef2375
Varun Venkitachalam <Varun.Venkitachalam@gmail.com>
2017-02-11 20:43:31 -0800

    adding tutorSignup.html, tutorSignup.css, and tutorSignup.js

---
+++ js/tutorSignup.js
@@ -0,0 +1,16 @@
+
+function read(){
+//<script type="text/javascript">
+//document.getElementById("userInput").addEventListener('click', function () {
+//     console.log("clicked");
+  //  var text = document.getElementById('text');
+    //text.val() = (text.val()+ ' after clicking');
+
+//</script>
+
+var initialInput = document.getElementById('subject-confirm').value;
+document.getElementById("text").value
+document.getElementById("text").value = document.getElementById("text").value + " " + initialInput;
+ //document.getElementById('subject-confirm').value;
+document.getElementById('subject-confirm').value='';
+}
